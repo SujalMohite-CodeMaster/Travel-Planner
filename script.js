0b3d1b021aab3d6188d8adc11a5ae1c10f6abea8
SujalMohite-CodeMaster <mohitesujal35@gmail.com>
2024-10-04 19:27:23 +0530

    modified file footer added

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function searchDestination() {
     const searchValue = document.getElementById("searchInput").value;
     if (searchValue) {
       alert(`Searching for trips to: ${searchValue}`);
-      // Here you can integrate API calls to fetch actual data
+      
     } else {
       alert("Please enter a destination.");
     }
